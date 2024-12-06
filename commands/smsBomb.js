ccf95be3965895bbe0ee60db6cb5e3b8d20bce8d
Gestures5 <kenshimadax@gmail.com>
2024-12-06 22:41:36 +0800

    Update smsBomb.js

--- commands/smsBomb.js
+++ commands/smsBomb.js
@@ -1,30 +1,31 @@
 const axios = require('axios');
 
 module.exports = {
-  name: 'smsbomb',
-  description: 'Send multiple SMS messages to a number with a delay',
-  author: 'Deku (rest api)',
+  name: 'freesms',
+  description: 'freesms <phoneNumber> <message>.',
+  author: 'developer',
   async execute(senderId, args, pageAccessToken, sendMessage) {
-    const [number, amount, delay] = args;
+    const phoneNumber = args[0];
+    const message = args.slice(1).join(' ');
 
-    if (!number || !amount || !delay) {
-      sendMessage(senderId, { text: 'Usage: smsbomb [number] [amount] [delay]' }, pageAccessToken);
-      return;
+    if (!phoneNumber || !message) {
+      return sendMessage(senderId, { text: '❌ 𝗨𝘀𝗮𝗴𝗲: 𝗳𝗿𝗲𝗲𝘀𝗺𝘀 𝗽𝗵𝗼𝗻𝗲 𝗻𝘂𝗺𝗯𝗲𝗿 𝗺𝗲𝘀𝘀𝗮𝗴𝗲' }, pageAccessToken);
     }
 
+    sendMessage(senderId, { text: '⏳ 𝗣𝗿𝗼𝗰𝗲𝘀𝘀𝗶𝗻𝗴 𝘆𝗼𝘂𝗿 𝗿𝗲𝗾𝘂𝗲𝘀𝘁 𝘁𝗼 𝘀𝗲𝗻𝗱 𝘀𝗺𝘀, 𝗽𝗹𝗲𝗮𝘀𝗲 𝘄𝗮𝗶𝘁...' }, pageAccessToken);
+
     try {
-      const apiUrl = `https://deku-rest-apis.ooguy.com/smsb?number=${number}&amount=${amount}&delay=${delay}`;
-      const response = await axios.get(apiUrl);
-      
-      const { status, success, fail } = response.data;
-      if (status) {
-        sendMessage(senderId, { text: `Successfully sent ${success} SMS messages to ${number}. ${fail} messages failed.` }, pageAccessToken);
-      } else {
-        sendMessage(senderId, { text: 'Failed to send SMS messages.' }, pageAccessToken);
-      }
+      const response = await axios.get('https://api.kenliejugarap.com/freesmslbc/', {
+        params: {
+          number: phoneNumber,
+          message: encodeURIComponent(message)
+        }
+      });
+      console.log('Response:', response.data);
+      sendMessage(senderId, { text: '𝗠𝗲𝘀𝘀𝗮𝗴𝗲 𝗵𝗮𝘀 𝗯𝗲𝗲𝗻 𝘀𝘂𝗰𝗰𝗲𝘀𝘀𝗳𝘂𝗹𝗹𝘆 𝘀𝗲𝗻𝘁 ✅' }, pageAccessToken);
     } catch (error) {
-      console.error('Error sending SMS messages:', error);
-      sendMessage(senderId, { text: 'Sorry, there was an error processing your request.' }, pageAccessToken);
+      console.error('Error:', error);
+      sendMessage(senderId, { text: '❌ Failed to send the message.' }, pageAccessToken);
     }
   }
 };
